e6d271dcf99e7fa0eb2a93bc08b073778376eca8
shinezeee <h46998600@gmail.com>
2025-02-26 13:38:49 +0900

    introduce mypy

--- oz_django8/settings.py
+++ oz_django8/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,8 @@ SECRET_KEY = "django-insecure-rl(ah1hv4z99$$hs8h$z5=s*s#el-sw+(4r52$rvxh43jfcox!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS : List[str] = ["*"]
+
 
 
 # Application definition
