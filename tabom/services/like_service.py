15735e16d2466bf31edb4cc3f1a0811bab04e5f7
shinezeee <h46998600@gmail.com>
2025-02-28 13:49:33 +0900

    like count, cancel

--- tabom/services/like_service.py
+++ tabom/services/like_service.py
@@ -1,5 +1,26 @@
+from django.core.exceptions import BadRequest
+from django.db import IntegrityError
+
 from tabom.models import Like
 
 
 def do_like(user_id: int, article_id: int) -> Like:
-    return Like.objects.create(user_id=user_id, article_id=article_id)
+    try:
+        return Like.objects.create(user_id=user_id, article_id=article_id)
+    except IntegrityError as e:
+        if "FOREIGN KEY (`user_id`)" in e.args[1]:
+            raise BadRequest(f"없는 user_id 입니다: {user_id}")
+        if "FOREIGN KEY (`article_id`)" in e.args[1]:
+            raise BadRequest(f"없는 article_id 입니다: {article_id}")
+        # if "중복에 의한 integrity error 검사" in e.args[1]:
+        #     raise BadRequest(f"이미 좋아요 하셨습니다.")
+        raise
+
+
+# def do_unlike(user_id: int, article_id: int) -> None:
+#     like = Like.objects.filter(user_id=user_id, article_id=article_id).get()
+#     like.delete()
+
+
+def do_unlike(user_id: int, article_id: int) -> None:
+    Like.objects.filter(user_id=user_id, article_id=article_id).delete()
--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -1,21 +1,21 @@
+from django.core.exceptions import BadRequest
 from django.db import IntegrityError
 from django.test import TestCase
 
-from tabom.models import Article, User
-from tabom.services.like_service import do_like
+from tabom.models import Article, Like, User
+from tabom.services.like_service import do_like, do_unlike
 
 
 class TestLikeService(TestCase):
     def test_a_user_can_like_an_article(self) -> None:
-        # Given : 테스트에 필요한 재료준비
+        # Given: 테스트에 필요한 재료를 준비한다.
         user = User.objects.create(name="test")
-        article = Article.objects.create(
-            title="test_title",
-        )
-        # When : 실제 테스트 대상이 되는 동작을 실행
+        article = Article.objects.create(title="test_title")
+
+        # When: 실제 테스트 대상이 되는 동작을 실행합니다.
         like = do_like(user.id, article.id)
 
-        # Then : 동작 마친후 결과가 예상한대로 나왓는지 검증
+        # Then: 동작을 마친후에 결과가 "에상한 대로" 나왔는지 검증합니다.
         self.assertIsNotNone(like.id)
         self.assertEqual(user.id, like.user_id)
         self.assertEqual(article.id, like.article_id)
@@ -29,3 +29,63 @@ class TestLikeService(TestCase):
         like1 = do_like(user.id, article.id)
         with self.assertRaises(IntegrityError):
             like2 = do_like(user.id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_user_does_not_exist(
+        self,
+    ) -> None:
+        # Given
+        invalid_user_id = 9988
+        article = Article.objects.create(title="test_title")
+
+        # Expect
+        with self.assertRaises(BadRequest) as e:
+            do_like(invalid_user_id, article.id)
+
+        self.assertEqual(str(e.exception), "없는 user_id 입니다: 9988")
+
+    def test_it_should_raise_exception_when_like_an_article_does_not_exist(
+        self,
+    ) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        invalid_article_id = 9988
+
+        # Expect
+        with self.assertRaises(BadRequest) as e:
+            do_like(user.id, invalid_article_id)
+
+        self.assertEqual(str(e.exception), "없는 article_id 입니다: 9988")
+
+    def test_like_count_should_increase(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+
+        # When
+        do_like(user.id, article.id)
+
+        # Then
+        article = Article.objects.get(id=article.id)
+        self.assertEqual(1, article.like_set.count())
+
+    def test_a_user_can_undo_like(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        article = Article.objects.create(title="test_title")
+        like = Like.objects.create(user_id=user.id, article_id=article.id)
+
+        # When
+        do_unlike(user.id, article.id)
+
+        # Then
+        with self.assertRaises(Like.DoesNotExist):
+            Like.objects.get(id=like.id)
+
+    # def test_it_should_raise_exception_when_undo_like_which_does_not_exist(self) -> None:
+    #     # Given
+    #     user = User.objects.create(name="test")
+    #     article = Article.objects.create(title="test_title")
+    #
+    #     # Expect
+    #     with self.assertRaises(Like.DoesNotExist):
+    #         do_unlike(user.id, article.id)
